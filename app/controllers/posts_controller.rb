f8312b447c6d43f48dd1b7e22e6629335cbc998b
olimboloic <olimbonzengueloicbertrand@gmail.com>
2019-04-24 00:15:14 +0100

    blog stylisé

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,35 @@ class PostsController < ApplicationController
         @posts = Post.all.order('created_at DESC')
     end
     def new
+        @post = Post.new
     end
     def show
     @post = Post.find(params[:id])
     end
     def create
         @post = Post.new(post_params)
-        @post.save
-
+        if @post.save
         redirect_to @post
+        else
+            render 'new'
+        end
+    end
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to root_path
+    end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        if  @post.update(params[:post].permit(:titre, :contenu))
+            redirect_to @post
+        else
+            render 'edit'
+        end
     end
     private
     def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :titre, presence: true, length: {minimum: 5}
+    validates :contenu, presence: true
 end
