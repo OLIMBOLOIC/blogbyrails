100ff85dbd72341ef3d9328a03aa058245610a76
olimboloic <olimbonzengueloicbertrand@gmail.com>
2019-04-29 18:26:29 +0100

    last maj

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 resources :posts do
     resources :commentaires
 end
 root "posts#index"
+get '/about', to: 'pages#about'
 end
 
--- db/migrate/20190428160101_add_utilisateur_id_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddUtilisateurIdToPost < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :utilisateur_id, :integer
-  end
-end
